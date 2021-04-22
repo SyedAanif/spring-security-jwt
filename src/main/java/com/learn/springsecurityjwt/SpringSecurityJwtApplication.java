82bf07fe6687582ba18a25264be0301b76776e41
Syed Aanif <syedaanif07@gmail.com>
2021-04-22 19:52:15 +0530

    Initial commit

---
+++ src/main/java/com/learn/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,39 @@
+package com.learn.springsecurityjwt;
+
+import com.learn.springsecurityjwt.filters.JwtRequestFilter;
+import com.learn.springsecurityjwt.models.AuthenticationRequest;
+import com.learn.springsecurityjwt.models.AuthenticationResponse;
+import com.learn.springsecurityjwt.service.MyUserDetailsService;
+import com.learn.springsecurityjwt.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
+
